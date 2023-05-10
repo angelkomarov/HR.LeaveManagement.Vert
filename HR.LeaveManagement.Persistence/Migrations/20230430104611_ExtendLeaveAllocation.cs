3530deb7091d9437b3d60c9d5f8904f47052b75a
Angel Komarov <angelkomarov@hotmail.com>
2023-05-11 09:04:25 +1000

    Added custom EF migration.

---
+++ HR.LeaveManagement.Persistence/Migrations/20230430104611_ExtendLeaveAllocation.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HR.LeaveManagement.Persistence.Migrations
+{
+    public partial class ExtendLeaveAllocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "LeaveAllocations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "LeaveAllocations");
+        }
+    }
+}
